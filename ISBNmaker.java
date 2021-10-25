81474f3d055df7c9faf636152dbd12e43c8eeaa7
Crazydill8 <90478552+Crazydill8@users.noreply.github.com>
2021-10-25 14:05:14 -0400

    Create ISBNmaker.java

---
+++ ISBNmaker.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ISBNmaker {
+	public void main(String[] args) {
+		ISBN();
+	}
+	
+	public void ISBN() { //takes inputs to create ISBN numbers
+		System.out.println("Insert an ISBN number");
+	}
+}
